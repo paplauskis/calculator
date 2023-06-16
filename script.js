ffea764824c80b68d29b0d6e5bd3767935e0ad0d
paplauskis <paplauskaskostas@gmail.com>
2023-06-16 13:30:49 +0300

    formatted code with prettier

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let displayValue = '';
-let storedNumber = '';
-let firstNumber = '';
-let secondNumber = '';
-let storedOperator = '';
-const display = document.querySelector('.screen');
-const clearBtn = document.querySelector('.clear');
-clearBtn.addEventListener('click', clear);
+let displayValue = ''
+let storedNumber = ''
+let firstNumber = ''
+let secondNumber = ''
+let storedOperator = ''
+const display = document.querySelector('.screen')
+const clearBtn = document.querySelector('.clear')
+clearBtn.addEventListener('click', clear)
 
 const calculator = (() => {
-  const add = (a, b) => a + b;
-  const subtract = (a, b) => a - b;
-  const multiply = (a, b) => a * b;
-  const divide = (a, b) => b === 0 ? 'Can\'t divide by 0!' : a / b;
+  const add = (a, b) => a + b
+  const subtract = (a, b) => a - b
+  const multiply = (a, b) => a * b
+  const divide = (a, b) => (b === 0 ? "Can't divide by 0!" : a / b)
 
   return {
     add,
@@ -19,74 +19,74 @@ const calculator = (() => {
     multiply,
     divide,
   }
-})();
+})()
 
 const operate = function (operator, num1, num2) {
-  if (operator === '+') return calculator.add(num1, num2);
-  if (operator === '-') return calculator.subtract(num1, num2); 
-  if (operator === '*') return calculator.multiply(num1, num2);
+  if (operator === '+') return calculator.add(num1, num2)
+  if (operator === '-') return calculator.subtract(num1, num2)
+  if (operator === '*') return calculator.multiply(num1, num2)
   if (operator === '/') {
-    if (num2 === 0) return 'ERROR';
-    return calculator.divide(num1, num2);
+    if (num2 === 0) return 'ERROR'
+    return calculator.divide(num1, num2)
   }
-  return 'Error: invalid operator!';
-};
+  return 'Error: invalid operator!'
+}
 
 function appendNumber(num) {
-  displayValue += num;
-  display.textContent = displayValue;
+  displayValue += num
+  display.textContent = displayValue
 }
 
 function clear() {
-  displayValue = '';
-  display.textContent = '0';
-  storedNumber = '';
-  firstNumber = '';
-  secondNumber = '';
-  storedOperator = '';
+  displayValue = ''
+  display.textContent = '0'
+  storedNumber = ''
+  firstNumber = ''
+  secondNumber = ''
+  storedOperator = ''
 }
 
-const numberBtn = document.querySelectorAll('.number');
+const numberBtn = document.querySelectorAll('.number')
 numberBtn.forEach((number) => {
   number.addEventListener('click', () => {
     if (storedOperator === '') {
-      firstNumber += number.value;
-      firstNumber *= 1;
+      firstNumber += number.value
+      firstNumber *= 1
     } else {
-      secondNumber += number.value;
-      secondNumber *= 1;
+      secondNumber += number.value
+      secondNumber *= 1
     }
-    appendNumber(number.textContent);
-  });
-});
+    appendNumber(number.textContent)
+  })
+})
 
-const operatorBtn = document.querySelectorAll('.operator');
+const operatorBtn = document.querySelectorAll('.operator')
 operatorBtn.forEach((operator) => {
   operator.addEventListener('click', () => {
     if (storedOperator === '') {
-      storedNumber = firstNumber;
+      storedNumber = firstNumber
     } else {
-      storedNumber = operate(storedOperator, firstNumber, secondNumber);
-      firstNumber = storedNumber;
-      secondNumber = '';
-      displayValue = firstNumber.toString();
-      display.textContent = displayValue;
+      storedNumber = operate(storedOperator, firstNumber, secondNumber)
+      firstNumber = storedNumber
+      secondNumber = ''
+      displayValue = firstNumber.toString()
+      display.textContent = displayValue
     }
-    storedOperator = operator.value;
-    displayValue = operator.textContent;
-  });
-});
+    storedOperator = operator.value
+    displayValue = operator.textContent
+  })
+})
 
-const equalBtn = document.querySelector('.equal');
-equalBtn.addEventListener('click', equal);
+const equalBtn = document.querySelector('.equal')
+equalBtn.addEventListener('click', equal)
 
 function equal() {
   firstNumber = display.textContent = operate(
     storedOperator,
     storedNumber,
-    secondNumber,
-  );
-  secondNumber = '';
-  storedNumber = '';
-  storedOperator = '';
+    secondNumber
+  )
+  secondNumber = ''
+  storedNumber = ''
+  storedOperator = ''
 }
